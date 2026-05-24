12719a26ba4ec06fea2b156b31fd83056ec88a41
yyheroi <crystalyyhero@gmail.com>
2026-05-25 00:57:45 +0800

    <feat>(trap): add S-mode vectored trap entry and dispatch
    Wire Arch static library with vectored trap table, per-hart SupervisorTrap,
    and CSRManager; use arch::trap namespace and PascalCase module layout.

--- src/Kernel/Init/init.cc
+++ src/Kernel/Init/init.cc
@@ -1,7 +1,29 @@
 
 #include "Kernel/Kernel.hh"
+#include "Trap/Trap.hh"
+#include "CSR/CSRManager.hh"
+
+volatile static int g_started= 0;
+
+extern "C" void KernelInit(void)
+{
+    auto &csr= arch::csr::Manager::Instance();
+
+    if(csr.Cpuid() == 0) {
+        arch::trap::SupervisorTrap::Init();
+        //std::printf("Hello, RVOS\n");
+        g_started= 1;
+    } else {
+        while(g_started == 0)
+            ;
+        arch::trap::SupervisorTrap::Init();
+        //printf("hart %d starting \n", arch::csr::Manager::Instance().Cpuid());
+    }
+}
 
 int main()
 {
+    arch::csr::Manager::Instance().StartSupervisorMode(&KernelInit);
     return Kernel::Instance().Run();
 }
+
